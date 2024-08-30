585f5aac80c760e3538c4e5e02bafc3643921145
Jagadeeshkavuri1 <145770710+Jagadeeshkavuri1@users.noreply.github.com>
2024-08-30 11:17:04 +0530

    Create tuple operation.py

---
+++ tuple operation.py
@@ -0,0 +1,26 @@
+t1=(1,2,3,4,5,6,7,8,9,10) 
+t2=(11,12,13,14,15,16) 
+print("tuple concentation ",t1+t2) 
+print("tuple repatitaion",t1*2) 
+print("index of 5 in tuple1",t1.index(5)) 
+print("membership operator",1 in t1) 
+print("membership operator",1 not in t1)  
+print("tuple literation ")
+for i in t1: 
+   print(i) 
+print("actual tuple is ",t1) 
+print("slice operators")   
+print(t1[1])   
+print(t1[1:6])
+print(t1[1:-1])
+print(t1[:3]) 
+print(t1[2:])  
+print(t1[:-1]) 
+print(t1[::-1]) 
+print(t1[::-2]) 
+print(t1[1::2])  
+print(t1[0::2]) 
+
+
+
+output 
